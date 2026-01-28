d7460cae3edea8e70154a5fa73cf633e7798119a
Ukshit Chauhan <chauhanukshit@gmail.com>
2026-01-28 13:09:18 +0530

    APIchange

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL:'http://localhost:5000/'
+    baseURL:'https://myapp-1-fvmj.onrender.com/'
 })
 
 API.interceptors.request.use((config) => {
